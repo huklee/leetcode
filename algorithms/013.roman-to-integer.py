113481339dbd98d210cdb0529ca6237855385342
huklee <lhwnotss@gmail.com>
2017-01-12 23:19:06 +0900

    updated the solution of 013.

--- algorithms/013.roman-to-integer.py
+++ algorithms/013.roman-to-integer.py
@@ -1,35 +1,20 @@
-from collections import OrderedDict
-
 class Solution(object):
     def romanToInt(self, s):
         """
         :type s: str
         :rtype: int
         """
+        roman_dict = {"C":100, "M":1000, "D":500, "C":100, "L":50, "X":10, "V":5, "I":1}
         result = 0
-        temp = s
-        roman_dict = OrderedDict()
-        roman_dict["CM"] = 900
-        roman_dict["M"] = 1000
-        roman_dict["CD"] = 400
-        roman_dict["D"] = 500
-        roman_dict["XC"] = 90
-        roman_dict["C"] = 100
-        roman_dict["XL"] = 40
-        roman_dict["L"] = 50
-        roman_dict["IX"] = 9
-        roman_dict["X"] = 10
-        roman_dict["IV"] = 4
-        roman_dict["V"] = 5
-        roman_dict["I"] = 1
-        for oneDict in roman_dict:
-            if oneDict not in temp:
-                continue
-            while oneDict in temp:
-                result += roman_dict[oneDict]
-                pos = temp.find(oneDict)
-                if len(oneDict) == 2:
-                    temp = temp[:pos] + "__" + temp[pos + 2:]
-                else:
-                    temp = temp[:pos] + "_" + temp[pos + 1:]            
+        for i in range(0, len(s) - 1):
+            prev_char = s[i]
+            next_char = s[i+1]
+            # 01. next char is bigger than previous one, this is the minus operation
+            if (roman_dict[prev_char] < roman_dict[next_char]):
+                result -= roman_dict[prev_char]
+            # 02. otherwise, it's addition
+            else:
+                result += roman_dict[prev_char]
+        # for the last char, just plus
+        result += roman_dict[s[len(s)-1]]
         return result
