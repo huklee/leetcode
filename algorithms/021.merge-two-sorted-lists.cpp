a3c942a2145903115847b7dfc0bfd24fca1629c6
huklee <lhwnotss@gmail.com>
2017-02-17 23:28:11 +0900

    updated the 021 recursive solution

--- algorithms/021.merge-two-sorted-lists.cpp
+++ algorithms/021.merge-two-sorted-lists.cpp
@@ -6,9 +6,28 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
 class Solution {
 public:
+	// ========================= Recursive solution ============================
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        // 00. case of one list is empty
+        if (l1 == nullptr)  return l2;
+        if (l2 == nullptr)  return l1;
+        
+        //01. other cases, choose the smaller value to connect
+        if (l1->val > l2->val){
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+        else{
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }
+    }
+
+	// ========================= Iterative Solution ============================
+    ListNode* mergeTwoLists_(ListNode* l1, ListNode* l2) {
         // 00. initialization Variables
         ListNode *p1 = l1, *p2 = l2, *result = nullptr, *tail = nullptr;
         ListNode **target = nullptr;
