683bb63f0ef932bbc6f5912caea855ce9c19790e
huklee <lhwnotss@gmail.com>
2017-04-09 11:21:59 +0900

    updated no.461

--- algorithms/461.hamming-distance.cpp
+++ algorithms/461.hamming-distance.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     int hammingDistance(int x, int y) {
         int z = x^y;
-        int count = 0, i = 1;
-        while (z){
-            if (i & z)
-                count++;
-            z &= ~i;
-            i <<= 1;
+        return sparse_bit(z);
+    }
+    
+    int sparse_bit(int x){
+        int count = 0;
+        while (x){
+            count++;
+            x &= (x - 1);
         }
         return count;
     }
