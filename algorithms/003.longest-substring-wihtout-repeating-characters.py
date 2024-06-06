96eb8554c95e18caa46700bc8a19d2bbf5fe8bb5
Hyeonguk Lee <lhwnotss@gmail.com>
2024-06-07 07:13:32 +0900

    Update 003.longest-substring-wihtout-repeating-characters.py
    Replace dictionary with set to optimize the solution.

--- algorithms/003.longest-substring-wihtout-repeating-characters.py
+++ algorithms/003.longest-substring-wihtout-repeating-characters.py
@@ -2,24 +2,24 @@
 # author: github.com/huklee
 # TC O(n) solution using a single hash map
 class Solution:
-    def dedup(self, s, start, end, charMap) -> int:
+    def dedup(self, s, start, end, charSet) -> int:
         for j in range(end - start):
             if s[start] == s[end]:
                 start += 1
                 break
             else: 
-                charMap[s[start]] = 0 
+                charSet.discard(s[start]) 
                 start += 1
         return start
 
     def lengthOfLongestSubstring(self, s: str) -> int:
         start, end, maxSubStr = 0, 0, 0
-        charMap = dict()
+        charSet = set()
         for i in range(len(s)):
             end = i
-            if s[i] not in charMap or charMap[s[i]] == 0:
-                charMap[s[i]] = 1
+            if s[i] not in charSet:
+                charSet.add(s[i])
                 maxSubStr = max(maxSubStr, end - start + 1)
             else:
-                start = self.dedup(s, start, end, charMap)
+                start = self.dedup(s, start, end, charSet)
         return maxSubStr
