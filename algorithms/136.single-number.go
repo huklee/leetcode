952a67b7bb1246f49c9a295c7fcaf51e1af9e1bf
huklee <lhwnotss@gmail.com>
2024-09-30 23:29:06 +0900

    Update 136.single-number.go

--- algorithms/136.single-number.go
+++ algorithms/136.single-number.go
@@ -1,8 +1,7 @@
-// singleNumber function finds the single number in the slice.
 func singleNumber(nums []int) int {
-    res := 0
-    for _, num := range nums {
-        res ^= num // Use XOR operation to eliminate the numbers that appear twice
-    }
-    return res
+    singleNumber := 0
+    for _, i := range nums {
+        singleNumber ^= i
+    } 
+    return singleNumber
 }
