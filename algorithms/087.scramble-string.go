35c93b6fb23c5f1fc983a72f7d2fc1325522fc03
huklee <lhwnotss@gmail.com>
2024-10-02 00:10:51 +0900

    Fix the 087 go solution to fit Leetcode

--- algorithms/087.scramble-string.go
+++ algorithms/087.scramble-string.go
@@ -1,56 +1,53 @@
-type Solution struct {
-	memoization [][][]*bool
-	str1, str2  []rune
-	length      int
-}
+var memoization [][][]*bool
+var str1, str2 []rune
 
-func (sol *Solution) isScramble(s1 string, s2 string) bool {
-	sol.str1 = []rune(s1)
-	sol.str2 = []rune(s2)
-	sol.length = len(s1)
-	sol.memoization = make([][][]*bool, sol.length)
-	for i := range sol.memoization {
-		sol.memoization[i] = make([][]*bool, sol.length)
-		for j := range sol.memoization[i] {
-			sol.memoization[i][j] = make([]*bool, sol.length+1)
+func isScramble(s1 string, s2 string) bool {
+	str1 = []rune(s1)
+	str2 = []rune(s2)
+	length := len(s1)
+	memoization = make([][][]*bool, length)
+	for i := range memoization {
+		memoization[i] = make([][]*bool, length)
+		for j := range memoization[i] {
+			memoization[i][j] = make([]*bool, length+1)
 		}
 	}
-	return sol.canScramble(0, 0, sol.length)
+	return canScramble(0, 0, length)
 }
 
-func (sol *Solution) canScramble(i, j, length int) bool {
+func canScramble(i, j, length int) bool {
 	// Base case: strings of length 1
 	if length == 1 {
-		sol.memoization[i][j][length] = new(bool)
-		*sol.memoization[i][j][length] = sol.str1[i] == sol.str2[j]
-		return *sol.memoization[i][j][length]
+		memoization[i][j][length] = new(bool)
+		*memoization[i][j][length] = str1[i] == str2[j]
+		return *memoization[i][j][length]
 	}
 
 	// If result is already computed
-	if sol.memoization[i][j][length] != nil {
-		return *sol.memoization[i][j][length]
+	if memoization[i][j][length] != nil {
+		return *memoization[i][j][length]
 	}
 
 	// Try splitting the strings in every possible way
 	for k := 1; k < length; k++ {
 		// Case 1: No swap
-		if sol.canScramble(i, j, k) && sol.canScramble(i+k, j+k, length-k) {
-			sol.memoization[i][j][length] = new(bool)
-			*sol.memoization[i][j][length] = true
+		if canScramble(i, j, k) && canScramble(i+k, j+k, length-k) {
+			memoization[i][j][length] = new(bool)
+			*memoization[i][j][length] = true
 			return true
 		}
 
 		// Case 2: With swap
-		if sol.canScramble(i, j+length-k, k) && sol.canScramble(i+k, j, length-k) {
-			sol.memoization[i][j][length] = new(bool)
-			*sol.memoization[i][j][length] = true
+		if canScramble(i, j+length-k, k) && canScramble(i+k, j, length-k) {
+			memoization[i][j][length] = new(bool)
+			*memoization[i][j][length] = true
 			return true
 		}
 	}
 
 	// If no valid split was found
-	sol.memoization[i][j][length] = new(bool)
-	*sol.memoization[i][j][length] = false
+	memoization[i][j][length] = new(bool)
+	*memoization[i][j][length] = false
 	return false
 }
 
