ecaa603702b2262b011832f49abbab67d6e0f2ba
huklee <lhwnotss@gmail.com>
2024-10-02 00:16:15 +0900

    Refactor the solution 087 go

--- algorithms/087.scramble-string.go
+++ algorithms/087.scramble-string.go
@@ -1,15 +1,14 @@
-var memoization [][][]*bool
-var str1, str2 []rune
+var memo [][][]*bool
+var str1, str2 string
 
 func isScramble(s1 string, s2 string) bool {
-	str1 = []rune(s1)
-	str2 = []rune(s2)
+    str1, str2 = s1, s2
 	length := len(s1)
-	memoization = make([][][]*bool, length)
-	for i := range memoization {
-		memoization[i] = make([][]*bool, length)
-		for j := range memoization[i] {
-			memoization[i][j] = make([]*bool, length+1)
+	memo = make([][][]*bool, length)
+	for i := range memo {
+		memo[i] = make([][]*bool, length)
+		for j := range memo[i] {
+			memo[i][j] = make([]*bool, length+1)
 		}
 	}
 	return canScramble(0, 0, length)
@@ -18,36 +17,36 @@ func isScramble(s1 string, s2 string) bool {
 func canScramble(i, j, length int) bool {
 	// Base case: strings of length 1
 	if length == 1 {
-		memoization[i][j][length] = new(bool)
-		*memoization[i][j][length] = str1[i] == str2[j]
-		return *memoization[i][j][length]
+		memo[i][j][length] = new(bool)
+		*memo[i][j][length] = str1[i] == str2[j]
+		return *memo[i][j][length]
 	}
 
 	// If result is already computed
-	if memoization[i][j][length] != nil {
-		return *memoization[i][j][length]
+	if memo[i][j][length] != nil {
+		return *memo[i][j][length]
 	}
 
 	// Try splitting the strings in every possible way
 	for k := 1; k < length; k++ {
 		// Case 1: No swap
 		if canScramble(i, j, k) && canScramble(i+k, j+k, length-k) {
-			memoization[i][j][length] = new(bool)
-			*memoization[i][j][length] = true
+			memo[i][j][length] = new(bool)
+			*memo[i][j][length] = true
 			return true
 		}
 
 		// Case 2: With swap
 		if canScramble(i, j+length-k, k) && canScramble(i+k, j, length-k) {
-			memoization[i][j][length] = new(bool)
-			*memoization[i][j][length] = true
+			memo[i][j][length] = new(bool)
+			*memo[i][j][length] = true
 			return true
 		}
 	}
 
 	// If no valid split was found
-	memoization[i][j][length] = new(bool)
-	*memoization[i][j][length] = false
+	memo[i][j][length] = new(bool)
+	*memo[i][j][length] = false
 	return false
 }
 
