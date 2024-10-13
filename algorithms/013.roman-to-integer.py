abc6d011836c36e2be972b0ebb3a4bb2d86e2d23
huklee <lhwnotss@gmail.com>
2024-10-14 00:09:52 +0900

    Update 013, 022 python solutions to align with py1.30

--- algorithms/013.roman-to-integer.py
+++ algorithms/013.roman-to-integer.py
@@ -1,9 +1,5 @@
 class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+    def romanToInt(self, s: str) -> int:
         roman_dict = {"C":100, "M":1000, "D":500, "C":100, "L":50, "X":10, "V":5, "I":1}
         result = 0
         for i in range(0, len(s) - 1):
--- algorithms/022.generate-parentheses_extra.py
+++ algorithms/022.generate-parentheses_extra.py
@@ -1,37 +1,19 @@
-def sub(n, memo, cntOpen, cntClose):
-	# 00. edge case for
-	if cntOpen > n or cntClose > n or cntOpen < cntClose:
-		return 0
-	if cntOpen == n and cntClose == n:
-		return 1 
-
-	# 01. recall the memo
-	if memo[cntOpen][cntClose] > 0:
-		return memo[cntOpen][cntClose]
-	else:
-		result = sub(n, memo, cntOpen + 1, cntClose) + sub(n, memo, cntOpen, cntClose + 1)
-	
-		# 02. save the memo & recursive call
-		memo[cntOpen][cntClose] = result
-		return result
-
-def solve_(n):
-	memo = [[-1 for x in range(n+1)] for i in range(n+1)]
-	memo[0][0] = 0
-	return sub(n, memo, 0, 0)
-
-def sub2(n, cntOpen, cntClose, s):
-	# 00. final case
-	if cntOpen == n and cntClose == n:
-		print s
-	
-	# 01. edge case
-	if cntOpen > n or cntClose > n or cntOpen < cntClose:
-		return
-	sub2(n, cntOpen + 1, cntClose, s + "(")
-	sub2(n, cntOpen, cntClose + 1, s + ")")
-
-def solve(n):
-	return sub2(n, 0, 0, "");	
-
-print(solve(3))
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+        self.sub(result, "", n, n)
+        return result
+    
+    def sub(self, result: List[str], current: str, cntOpen: int, cntClose: int):
+        # Base case: if both counts reach zero, add the current combination to results
+        if cntOpen == 0 and cntClose == 0:
+            result.append(current)
+            return
+        
+        # Add an open parenthesis if we can
+        if cntOpen > 0:
+            self.sub(result, current + "(", cntOpen - 1, cntClose)
+        
+        # Add a close parenthesis if it doesn't exceed the number of open parentheses
+        if cntClose > cntOpen:
+            self.sub(result, current + ")", cntOpen, cntClose - 1)
